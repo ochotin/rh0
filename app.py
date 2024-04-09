941360a9fb1c91d97a81fb703bf2599fd3a60ee1
Olivier Chotin <olivier.chotin@ocia.fr>
2024-04-09 15:02:21 +0200

    Signed-off-by: Olivier Chotin <olivier.chotin@ocia.fr>

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, Request
 import numpy as np
 import pandas as pd
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World2"}
+	
+if __name__ == '__main__':
+    uvicorn.run('app:app', host='0.0.0.0', port=8000)
  
 
